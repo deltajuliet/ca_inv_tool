52dde502614c69d6f1abf4d3884f430b30251c76
Dan Jones <djones@n1nja.local>
2012-10-29 21:22:04 -0600

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CaInvTool::Application.config.secret_token = '829a1cea5f0b47e6743a68e01d601e7aefedb0d29742aebf4390bb9e0435f3992f2f2227a064aca449dca26655df8935651f3635ca4f4a0936957654fbdac915'
