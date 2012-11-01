290c6e4d57bdfcbce3abeafbe5f68929021d47a3
djones <dan@silentshadow.net>
2012-11-01 02:43:13 -0600

    Coding inventory scanner - Works!

--- app/controllers/scan_items_controller.rb
+++ app/controllers/scan_items_controller.rb
@@ -24,16 +24,17 @@ class ScanItemsController < ApplicationController
   # GET /scan_items/new
   # GET /scan_items/new.json
   def new
-    puts @scan_id = params[:parent_scan_id].to_i
+    @scan_id = params[:parent_scan_id].to_s.to_i
+    @message = params[:message].to_s unless params[:message].nil?
+    @item_in_inv = params[:item_in_inv].to_s unless params[:item_in_inv].nil?
     @scan_item = ScanItem.new
-    @scan_item.import_scan(@scan_id)
 
-    render 'scan_items/new'
+    #render 'scan_items/new'
 
-    #respond_to do |format|
-    #  format.html # new.html.erb
-    #  format.json { render :json => @scan_item }
-    #end
+    respond_to do |format|
+      format.html # new.html.erb
+      format.json { render :json => @scan_item }
+    end
   end
 
   # GET /scan_items/1/edit
@@ -44,17 +45,47 @@ class ScanItemsController < ApplicationController
   # POST /scan_items
   # POST /scan_items.json
   def create
-    puts @scan_id = params[:parent_scan_id].to_i
-    @scan_item = ScanItem.new(params[:scan_item])
+    @scan_id = params[:parent_scan_id].to_s.to_i
+    already_scanned = false
+
+    puts "DAN " * 6
+    scanned_sku = params["scan_item"]["item_sku"]
+    scanned_items = ScanItem.find_all_by_scan_id(@scan_id)
+
+    @scan_item = nil
+
+    scanned_items.each do |i|
+      @scan_item = i if i.item_sku == scanned_sku
+    end
+
+    puts @scan_item.inspect
+
+    if @scan_item.nil?
+      @scan_item = ScanItem.new
+      @scan_item.item_sku = scanned_sku
+      @scan_item.quantity = 1
+      @scan_item.scan_id = @scan_id
+    else
+      already_scanned = true
+      @scan_item.quantity += 1
+    end
+
+    @scan_item.in_inventory = false
+    @scan_item.message = "NOT in Inventory! (#{@scan_item.item_sku})"
+    inventory_items = InventoryItem.all
+
+    inventory_items.each do |item|
+      if item.inventory_number == @scan_item.item_sku
+        @scan_item.in_inventory = true
+        puts @scan_item.message = "Item Exists in Inventory: #{ScanItem.get_item_title(@scan_item.item_sku)}"
+      end
+    end
+
+    @scan_item.save
 
     respond_to do |format|
-      if @scan_item.save
-        format.html { redirect_to @scan_item, :notice => 'Scan item was successfully created.' }
+        format.html { redirect_to new_scan_item_path(:parent_scan_id => @scan_id, :message => @scan_item.message, :item_in_inv => @scan_item.in_inventory), :notice => 'Scan item was successfully created.' }
         format.json { render :json => @scan_item, :status => :created, :location => @scan_item }
-      else
-        format.html { render :action => "new" }
-        format.json { render :json => @scan_item.errors, :status => :unprocessable_entity }
-      end
     end
   end
 
@@ -77,11 +108,12 @@ class ScanItemsController < ApplicationController
   # DELETE /scan_items/1
   # DELETE /scan_items/1.json
   def destroy
+    @scan_id = params[:parent_scan_id].to_s.to_i
     @scan_item = ScanItem.find(params[:id])
     @scan_item.destroy
 
     respond_to do |format|
-      format.html { redirect_to scan_items_url }
+      format.html { redirect_to scans_url }
       format.json { head :no_content }
     end
   end
--- app/models/scan_item.rb
+++ app/models/scan_item.rb
@@ -1,25 +1,10 @@
 class ScanItem < ActiveRecord::Base
   attr_accessible :in_inventory, :item_sku, :quantity, :scan_id
-  attr_accessor :auction_title
+  attr_accessor :auction_title, :message
 
   belongs_to :scan
 
-  def import_scan(scan_id)
-
-  end
-
-  def item_exists?
-    @in_inventory = false
-    inventory_items = InventoryItem.all
-
-    inventory_items.each do |item|
-      if item.inventory_number == @item_sku
-        @in_inventory = true
-      end
-    end
-  end
-
-  def get_item_title
-    @auction_title = InventoryItem.find_by_inventory_number(@item_sku).auction_title unless @item_sku.nil?
+  def self.get_item_title(sku)
+    @auction_title = InventoryItem.find_by_inventory_number(sku).auction_title unless InventoryItem.find_by_inventory_number(sku).nil?
   end
 end
