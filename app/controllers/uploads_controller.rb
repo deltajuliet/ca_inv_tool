388bdfad39930342374552f4280544eef1dcfa5b
djones <dan@silentshadow.net>
2012-10-31 18:39:34 -0600

    Inventory file uploader now works

--- app/controllers/uploads_controller.rb
+++ app/controllers/uploads_controller.rb
@@ -7,9 +7,17 @@ class UploadsController < ApplicationController
       format.json { render :json => @upload }
     end
   end
+
   def create
     @upload = Upload.new
     post = @upload.parse_inventory_file(params[:upload])
-    render :inline => "<p>File has been uploaded successfully<br /><br /><%= link_to 'Go Home', welcome_index_path %></p>"
+    render :inline => "<p>File has been uploaded successfully<br /><br />Please wait while inventory is added to database...</p>"
+    @upload.add_items_to_database
+    @upload.cleanup
+    render :inline => "<p>Inventory added successfully<br /><br /><%= link_to 'Go Home', welcome_index_path %></p>"
+  end
+
+  def add_inventory
+
   end
 end
--- app/models/upload.rb
+++ app/models/upload.rb
@@ -6,20 +6,25 @@ class Upload < ActiveRecord::Base
     directory = "public/data"
     # create the file path
     path = File.join(directory, name)
-    puts "DAN!!! Path is: #{path}"
+    @filename = path
     # write the file
     File.open(path, "wb") { |f| f.write(upload['upload'].read) }
-    add_items_to_database(path)
-    File.delete(path)
   end
 
-  def add_items_to_database(filepath)
+  def add_items_to_database
     ActiveRecord::Base.connection.execute("TRUNCATE inventory_items")
     ActiveRecord::Base.connection.execute("ALTER SEQUENCE inventory_items_id_seq RESTART WITH 1")
 
-    inv_file = File.open(filepath, 'r')
+    inv_file = File.open(@filename, 'r')
 
-    first_line = inv_file.first
+    converted_inv_file = []
+
+    inv_file.each do |line|
+      conv_line = line.encode('UTF-16le', :invalid => :replace, :replace => '').encode('UTF-8')
+      converted_inv_file.push(conv_line)
+    end
+
+    first_line = converted_inv_file.shift
 
     header_hash = {
         :auction_title => nil,
@@ -45,35 +50,85 @@ class Upload < ActiveRecord::Base
     }
 
     column_counter = 0
-    first_line.each do |column|
+    puts first_line.inspect
+    first_line.split(/\t/).each do |column|
       case column
-        when "Auction Title"
+        when /.Auction Title|Auction Title/
           header_hash[:auction_title] = column_counter
         when "Inventory Number"
+          header_hash[:inventory_number] = column_counter
         when "Total Quantity"
+          header_hash[:total_quantity] = column_counter
         when "Weight"
+          header_hash[:weight] = column_counter
         when "UPC"
+          header_hash[:upc] = column_counter
         when "ASIN"
+          header_hash[:asin] = column_counter
         when "MPN"
+          header_hash[:mpn] = column_counter
         when "Description"
+          header_hash[:description] = column_counter
         when "Brand"
+          header_hash[:brand] = column_counter
         when "Starting Bid"
+          header_hash[:starting_bid] = column_counter
         when "Seller Cost"
+          header_hash[:seller_cost] = column_counter
         when "Buy It Now Price"
+          header_hash[:buy_it_now_price] = column_counter
         when "Retail Price"
+          header_hash[:retail_price] = column_counter
         when "ChannelAdvisor Store Price"
+          header_hash[:channeladvisor_store_price] = column_counter
         when "Second Chance Offer Price"
+          header_hash[:second_chance_offer_price] = column_counter
         when "Picture URLs"
+          header_hash[:picture_urls] = column_counter
         when "Received In Inventory"
+          header_hash[:received_in_inventory] = column_counter
         when "Labels"
+          header_hash[:labels] = column_counter
         when "Flag"
+          header_hash[:flag] = column_counter
         when "Classification"
+          header_hash[:classification] = column_counter
       end
       column_counter += 1
     end
 
-    inv_file.each_line do |line|
-      puts line
+    converted_inv_file.each do |line|
+      split_line = line.split(/\t/)
+
+      item = InventoryItem.new
+      
+      item.auction_title = split_line[header_hash[:auction_title]]
+      item.inventory_number = split_line[header_hash[:inventory_number]]
+      item.total_quantity = split_line[header_hash[:total_quantity]]
+      item.weight = split_line[header_hash[:weight]] unless header_hash[:weight].nil?
+      item.upc = split_line[header_hash[:upc]] unless header_hash[:upc].nil?
+      item.asin = split_line[header_hash[:asin]] unless header_hash[:asin].nil?
+      item.mpn = split_line[header_hash[:mpn]] unless header_hash[:mpn].nil?
+      item.description = split_line[header_hash[:description]] unless header_hash[:description].nil?
+      item.brand = split_line[header_hash[:brand]] unless header_hash[:brand].nil?
+      item.starting_bid = split_line[header_hash[:starting_bid]] unless header_hash[:starting_bid].nil?
+      item.seller_cost = split_line[header_hash[:seller_cost]] unless header_hash[:seller_cost].nil?
+      item.buy_it_now_price = split_line[header_hash[:buy_it_now_price]] unless header_hash[:buy_it_now_price].nil?
+      item.retail_price = split_line[header_hash[:retail_price]] unless header_hash[:retail_price].nil?
+      item.channel_advisor_store_price = split_line[header_hash[:channeladvisor_store_price]] unless header_hash[:channeladvisor_store_price].nil?
+      item.second_chance_offer_price = split_line[header_hash[:second_chance_offer_price]] unless header_hash[:second_chance_offer_price].nil?
+      item.picture_urls = split_line[header_hash[:picture_urls]] unless header_hash[:picture_urls].nil?
+      item.received_in_inventory = split_line[header_hash[:received_in_inventory]] unless header_hash[:received_in_inventory].nil?
+      item.labels = split_line[header_hash[:labels]] unless header_hash[:labels].nil?
+      item.flag = split_line[header_hash[:flags]] unless header_hash[:flags].nil?
+      item.classification = split_line[header_hash[:classification]] unless header_hash[:classification].nil?
+
+      item.save
     end
   end
+
+  def cleanup
+    File.delete(@filename)
+  end
+
 end
